181c11df2e40cf602c92ff90af37a6c13d3d1f69
zbrachinara <zbrachinara@gmail.com>
2024-11-23 10:22:09 -0500

    add more game options in ktris and lektris

--- NpTetris/Board.lean
+++ NpTetris/Board.lean
@@ -6,7 +6,7 @@ abbrev Board (n m : ℕ) := Finset (Fin n × Fin m)
 
 namespace Board
 
-variable {n m : ℕ}
+variable {n m : ℕ+}
 
 def position_map : Fin n × Fin m ↪ Position where
 toFun := by rintro ⟨p,q⟩; exact ⟨p,q⟩
@@ -18,19 +18,13 @@ inj' := by
 
 def points (board : Board n m) : Finset Position := board.map position_map
 
-/-- The difference between the initial and final board. These are the positions of the board which
-  are filled in the final board, but empty in the initial board. Concretely, this is a set
-  difference. -/
-instance : HSub (Board n m) (Board n m) (Finset Position) where
-hSub final initial := (final \ initial).points
-
-/-- If these conditions are satisfied, then a mino with the given shape can step the board from the
-  initial state to the final state. -/
-structure step {k} {path : @KMino.Path k} {maneuver : KMino.Maneuver path}
+/-- If these conditions are satisfied, then a mino with this shape can maneuver through the path and
+  lock onto the board to step it to the final state -/
+structure lock {k} {path : @KMino.Path k} {maneuver : KMino.Maneuver path}
   (shape : KShape k) (initial final : Board n m)
 where
--- /-- The difference between the two boards is exactly where the path ends -/
--- diff_correct : maneuver.last.points = final - initial
+/-- The locking position of the path completes the board -/
+diff_correct : maneuver.last.points ∪ initial.points = final.points
 /-- The shape of the mino described in the maneuver is exactly the one being considered. -/
 shape_correct : maneuver.head.shape = shape
 /-- The path begins at the top of the board -/
@@ -38,21 +32,38 @@ spawn_at_top : maneuver.head.max_height = m
 /-- No point on the path intersects `initial` -/
 no_intersections (m : @KMino k) : m ∈ path → m.points ∩ initial.points = ∅
 
-#print axioms step
+/-- A board can step from initial to final if the final board is a line clear on row `r` -/
+structure clear (r) (initial final : Board n m) where
+lower_id : ∀ c r', r' < r → (c, r') ∈ initial → (c, r') ∈ final
+/-- The rows `r` and above of `initial` should be the same as the -/
+upper_shifted : ∀ c r', r' > r → (c, r') ∈ initial → (c, r' - 1) ∈ final
+/-- Row `r` in the initial board should be full. -/
+r_full : ∀ c, (c, r) ∈ initial
+/-- The top of the final board should be empty. -/
+top_empty : ∀ c, (c, -1) ∉ final
 
 end Board
 
 /-- A game of k-tris parameterized by its queue, initial state, and final state -/
-inductive KTris {n m k} : List (KShape k) → Board n m → Board n m → Prop where
+inductive KTris {k} {n m : ℕ+} : List (KShape k) → Board n m → Board n m → Prop where
+/-- An empty queue does nothing -/
 | triv board : KTris [] board board
-| step shape steps b₁ b₂ b₃ : KTris steps b₁ b₂ → b₂.step shape b₃ → KTris (shape :: steps) b₁ b₃
+/-- Even without a piece, the board can step if it has a full row -/
+| clear queue initial row final : initial.clear row final → KTris queue initial final
+/-- If there is a piece in the queue, then it can lock down. -/
+| lock shape steps b₁ b₂ b₃ : KTris steps b₁ b₂ → b₂.lock shape b₃ → KTris (shape :: steps) b₁ b₃
 
 /-- A game of ≤k-tris parameterized by its queue, initial state, and final state. Unlike k-tris,
-  pieces can be of any size less than `k`, so many, many more conditions are possible. -/
-inductive LeKTris {n m k} : List (LeKShape k) → Board n m → Board n m → Prop where
+  pieces can be of any size less than `k`, and not just of size `k` itself. -/
+inductive LeKTris {k} {n m : ℕ+} : List (LeKShape k) → Board n m → Board n m → Prop where
+/-- An empty queue does nothing -/
 | triv board : LeKTris [] board board
-| step (lekshape : LeKShape k) kshape steps b₁ b₂ b₃ :
+/-- Even without a piece, the board can step if it has a full row -/
+| clear queue initial row final : initial.clear row final → LeKTris queue initial final
+/-- If there is a piece in the queue, then it can lock down. This is defined by finding the related
+  k-mino, and using that to find out what the final state of the board can be. -/
+| lock (lekshape : LeKShape k) kshape steps b₁ b₂ b₃ :
   LeKTris steps b₁ b₂ →
   lekshape.shape_eq kshape →
-  b₂.step kshape b₃ →
+  b₂.lock kshape b₃ →
   LeKTris (lekshape :: steps) b₁ b₃
